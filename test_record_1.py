462590b7b6f1d46fea392e368bf1effbdac92cdc
Hữu Tuấn Anh <huutuananh1406@gmail.com>
2024-07-25 11:07:26 +0700

    Show preview

--- test_record_1.py
+++ test_record_1.py
@@ -1,7 +1,6 @@
 import os
 
 import cv2
-from datetime import datetime
 
 # import numpy as np
 
@@ -54,15 +53,12 @@ def get_video_type(filename):
 cap = cv2.VideoCapture(0)
 out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
 
-now = datetime.now()
 while True:
     ret, frame = cap.read()
     out.write(frame)
-    if (datetime.now() - now).seconds > 10:
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-    # cv2.imshow("frame", frame)
-    # if cv2.waitKey(1) & 0xFF == ord("q"):
-    # break
 
 
 cap.release()
