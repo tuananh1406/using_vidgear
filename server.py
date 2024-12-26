c5864c33f2a204beb8c6078c81defaad2fecdfba
Hữu Tuấn Anh <huutuananh1406@gmail.com>
2024-12-26 15:53:15 +0700

    Reverse client - server

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-import socket, cv2, pickle, struct
+import socket
+import cv2
+import pickle
+import struct
 
 # Socket Create
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,26 +18,26 @@ server_socket.bind(socket_address)
 server_socket.listen(5)
 print("LISTENING AT:", socket_address)
 client_socket = None
-vid = None
 
-# Socket Accept
+data = b""
+payload_size = struct.calcsize("Q")
 while True:
-    try:
-        client_socket, addr = server_socket.accept()
-        print("GOT CONNECTION FROM:", addr)
-        if client_socket:
-            print("open camera")
-            vid = cv2.VideoCapture(0)
+    while len(data) < payload_size:
+        if packet := client_socket.recv(4 * 1024):
+            data += packet
+        else:
+            break
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-            while vid.isOpened():
-                img, frame = vid.read()
-                a = pickle.dumps(frame)
-                message = struct.pack("Q", len(a)) + a
-                client_socket.sendall(message)
-    except Exception as e:
-        print(e)
-        if client_socket:
-            client_socket.close()
-        if vid:
-            vid.release()
-        continue
+    while len(data) < msg_size:
+        data += client_socket.recv(4 * 1024)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("RECEIVING VIDEO", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+client_socket.close()
